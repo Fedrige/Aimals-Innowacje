b45d8420585579e7bc44ee807078eb067e345e11
fedrige <fedrige@gmail.com>
2022-07-11 09:23:57 +0200

    Create flow and fixing animal finder, and all else

--- force-app/main/default/lwc/lookForAnimal/lookForAnimal.js
+++ force-app/main/default/lwc/lookForAnimal/lookForAnimal.js
@@ -1,26 +1,28 @@
-/**
- * Created by Adam on 11.06.2022.
- */
-
-import {LightningElement, wire} from 'lwc';
+import {LightningElement, wire, api, track} from 'lwc';
 import ACCOUNT from '@salesforce/schema/Account'
 import getShelterNames from '@salesforce/apex/lookForAnimalController.getShelterNames'
 import getBreedNames from '@salesforce/apex/lookForAnimalController.getBreedNames'
 import getAge from '@salesforce/apex/lookForAnimalController.getAge'
 import getAnimal from '@salesforce/apex/lookForAnimalController.getAnimal'
+import getBreedOfAnimal from '@salesforce/apex/Einstein.getBreedOfAnimal'
 
 
 export default class LookForAnimal extends LightningElement {
     value = 'Any';
     shelterPicklistOptions = [];
     shelterValue = 'Any';
+    @track
     breedPicklistOptions = [];
     breedValue = 'Any';
     agePicklistOptions = [];
     ageValue = 'Any';
     genderValue = 'Any';
     animalsList;
+    base64String='';
     error;
+    fileData;
+    @api
+    myRecordId;
 
     @wire(getAnimal, {shelterValue: '$shelterValue', breedValue: '$breedValue', ageValue: '$ageValue', genderValue: '$genderValue'})
     wiredAnimals({error, data}){
@@ -31,6 +33,10 @@ export default class LookForAnimal extends LightningElement {
             this.error = error;
             this.animalsList = undefined;
         }
+    }
+
+    get acceptedFormats() {
+        return ['.pdf', '.png', '.jpg', '.jpeg'];
     }
 
     get shelterOptions() {
@@ -83,7 +89,51 @@ export default class LookForAnimal extends LightningElement {
     handlePicklistGenderChange(event){
         this.genderValue = event.target.value;
     }
-    handleClick(event){
-        //EINSTEIN VISSION
+
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+
     }
+
+    handleFilesChange(event){
+        const file = event.detail.files[0]
+        console.log(file)
+        const reader = new FileReader();
+        reader.onload =()=>{
+            const Base64 = reader.result.split(',')[1];
+
+            //this.breedValue = getBreedOfAnimal(base64)
+            getBreedOfAnimal({strBase64: Base64}).then(result =>{
+                const pickListBreed = this.template.querySelector('.breed')
+                if (pickListBreed){
+                    pickListBreed.value = result
+                }
+                this.breedValue = result
+                console.log(result)
+            })
+            this.fileData = {
+                'filename':file.name,
+                'base64':Base64,
+                'recordId':this.myRecordId
+            }
+            console.log(this.fileData)
+        }
+        reader.readAsDataURL(file)
+    }
+
+
+//function imageUploaded(imageToConvert) {
+//    var file = imageToConvert;
+//
+//    var reader = new FileReader();
+//
+//    reader.onload = function () {
+//        this.base64String = reader.result.replace('data:', '')
+//            .replace(/^.+,/, '');
+//
+//
+//        console.log(this.base64String);
+//    }
+//    reader.readAsDataURL(file);
+//}
 }
